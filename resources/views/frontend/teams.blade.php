ad82ec672344ccff72fdc8633351e8697e4a52f9
Sk Abir <abirshishir1996@gmail.com>
2022-11-02 14:14:37 +0600

    Bug Fix. teamrelated

--- resources/views/frontend/teams.blade.php
+++ resources/views/frontend/teams.blade.php
@@ -1,46 +1,10 @@
 @extends('layouts.frontend')
-@section('title') Teams | {{setting('website_name')}} @endsection
+@section('title') Teams | Shataphool Bangladesh @endsection
 @section('content')
 @include('includes.banner',['programName'=>'Teams'])
 <div id="content">
 
     <!-- WHO WE ARE -->
-    <!-- WHO WE ARE -->
-    @if($teams->where('type','Advisor')->count()>0)
-    <section class="team team-wrap padding-top-70 padding-bottom-20">
-        <div class="container">
-
-            <div class="text-center">
-                <h4 class="primary-color">Advisor</h4>
-                <hr>
-                <ul class="row items">
-                    @foreach ($teams as $team)
-                    @if ($team->type == "Advisor")
-
-
-                    <li class="col-md-4 item market">
-                        <article class="text-left"> <img class="img-responsive" src="{{asset('team_images/'.$team->image)}}" alt="">
-                            <h5>{{$team->name}}</h5>
-                            <p>{{$team->designation}}</p>
-                            <p>{!!$team->about!!}</p>
-                            <ul class="social">
-                                <li><a href="tel:{{$team->phone}}"><i class="fas fa-phone-square-alt"></i>&nbsp;{{$team->phone}}</a></li>
-                                <li><a href="mailto:{{$team->email}}"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;{{$team->email}}</a></li>
-                            </ul>
-                        </article>
-                    </li>
-                    @endif
-                    @endforeach
-
-                </ul>
-            </div>
-
-        </div>
-    </section>
-    @endif
-
-
-
     @if($teams->where('type','General Committee')->count()>0)
     <section class="team team-wrap padding-top-70 padding-bottom-20">
         <div class="container">
@@ -74,16 +38,49 @@
     </section>
     @endif
 
-    @if($teams->where('type','Executive Committee')->count()>0)
+    @if($teams->where('type','Advisor')->count()>0)
     <section class="team team-wrap padding-top-70 padding-bottom-20">
         <div class="container">
 
             <div class="text-center">
-                <h4 class="primary-color">EXECUTIVE COMMITTEE</h4>
+                <h4 class="primary-color">Advisor</h4>
                 <hr>
                 <ul class="row items">
                     @foreach ($teams as $team)
-                    @if ($team->type == "Executive Committee")
+                    @if ($team->type == "Advisor")
+
+
+                    <li class="col-md-3 item market">
+                        <article class="text-left"> <img class="img-responsive" src="{{asset('team_images/'.$team->image)}}" alt="">
+                            <h5>{{$team->name}}</h5>
+                            <p>{{$team->designation}}</p>
+                            <p>{!!$team->about!!}</p>
+                            <ul class="social">
+                                <li><a href="tel:{{$team->phone}}"><i class="fas fa-phone-square-alt"></i>&nbsp;{{$team->phone}}</a></li>
+                                <li><a href="mailto:{{$team->email}}"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;{{$team->email}}</a></li>
+                            </ul>
+                        </article>
+                    </li>
+                    @endif
+                    @endforeach
+
+                </ul>
+            </div>
+
+        </div>
+    </section>
+    @endif
+
+    @if($teams->where('type','Director')->count()>0)
+    <section class="team team-wrap padding-top-70 padding-bottom-20">
+        <div class="container">
+
+            <div class="text-center">
+                <h4 class="primary-color">Director</h4>
+                <hr>
+                <ul class="row items">
+                    @foreach ($teams as $team)
+                    @if ($team->type == "Director")
 
 
                     <li class="col-md-3 item market">
@@ -112,7 +109,7 @@
         <div class="container">
 
             <div class="text-center">
-                <h4 class="primary-color">HIGHER MANAGEMENT</h4>
+                <h4 class="primary-color">Higher Management</h4>
                 <hr>
                 <ul class="row items">
                     @foreach ($teams as $team)
@@ -139,76 +136,6 @@
         </div>
     </section>
     @endif
-
-    @if($teams->where('type','Head Office')->count()>0)
-    <section class="team team-wrap padding-top-70 padding-bottom-20">
-        <div class="container">
-
-            <div class="text-center">
-                <h4 class="primary-color">HEAD OFFICE</h4>
-                <hr>
-                <ul class="row items">
-                    @foreach ($teams as $team)
-                    @if ($team->type == "Head Office")
-
-
-                    <li class="col-md-3 item market">
-                        <article class="text-left"> <img class="img-responsive" src="{{asset('team_images/'.$team->image)}}" alt="">
-                            <h5>{{$team->name}}</h5>
-                            <p>{{$team->designation}}</p>
-                            <p>{!!$team->about!!}</p>
-                            <ul class="social">
-                                <li><a href="tel:{{$team->phone}}"><i class="fas fa-phone-square-alt"></i>&nbsp;{{$team->phone}}</a></li>
-                                <li><a href="mailto:{{$team->email}}"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;{{$team->email}}</a></li>
-                            </ul>
-                        </article>
-                    </li>
-                    @endif
-                    @endforeach
-
-                </ul>
-            </div>
-
-        </div>
-    </section>
-    @endif
-
-
-    
-    @if($teams->where('type','Field')->count()>0)
-    <section class="team team-wrap padding-top-70 padding-bottom-20">
-        <div class="container">
-
-            <div class="text-center">
-                <h4 class="primary-color">FIELD</h4>
-                <hr>
-                <ul class="row items">
-                    @foreach ($teams as $team)
-                    @if ($team->type == "Field")
-
-
-                    <li class="col-md-3 item market">
-                        <article class="text-left"> <img class="img-responsive" src="{{asset('team_images/'.$team->image)}}" alt="">
-                            <h5>{{$team->name}}</h5>
-                            <p>{{$team->designation}}</p>
-                            <p>{!!$team->about!!}</p>
-                            <ul class="social">
-                                <li><a href="tel:{{$team->phone}}"><i class="fas fa-phone-square-alt"></i>&nbsp;{{$team->phone}}</a></li>
-                                <li><a href="mailto:{{$team->email}}"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;{{$team->email}}</a></li>
-                            </ul>
-                        </article>
-                    </li>
-                    @endif
-                    @endforeach
-
-                </ul>
-            </div>
-
-        </div>
-    </section>
-    @endif
-
-
     @if($teams->where('type','Team Member')->count()>0)
     <section class="team team-wrap padding-top-70 padding-bottom-20">
         <div class="container">
@@ -241,5 +168,6 @@
         </div>
     </section>
     @endif
+
 </div>
 @endsection
